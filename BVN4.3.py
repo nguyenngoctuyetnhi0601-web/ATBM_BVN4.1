520c74cb60d675cf1fe9d6dd55f1ec2944bddf9b
tuyetnhi0601 <nguyenngoctuyetnhi0601@gmail.com>
2025-09-23 19:47:21 +0700

    Add files via upload

---
+++ BVN4.3.py
@@ -0,0 +1,18 @@
+#Trong thuật toán RSA, cho p=17, q=23, e=5. 
+#Thực hiện mã hóa thông điệp P=TenCuaBan.
+p = 17
+q = 23
+e = 5
+n = p * q
+
+#Thông điệp cần mã hóa
+P = "NguyenNgocTuyetNhi"
+
+#Đổi sang số ASCII
+M = [ord(c) for c in P]
+#Mã hóa RSA: C = M^e mod n
+C = [pow(m, e, n) for m in M]
+
+print("P:", P)
+print("ASCII:", M)
+print("RSA:", C)
